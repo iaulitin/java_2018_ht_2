75d06fecbea460e69e39caae04e91f5e7e9c5c72
pavelsitnikov <pavelsitn@gmail.com>
2018-10-04 07:13:36 +0300

    added few documentation to SimpleGood

--- src/ru/milandr/courses/farm/sitnikov/goods/SimpleGood.java
+++ src/ru/milandr/courses/farm/sitnikov/goods/SimpleGood.java
@@ -3,8 +3,20 @@ package ru.milandr.courses.farm.sitnikov.goods;
 import ru.milandr.courses.farm.Good;
 
 public abstract class SimpleGood implements Good {
+
+    /**
+     * Indicates that good is rotten.
+     */
     private boolean isRotten = false;
+
+    /**
+     * Indicates that good is going rotten.
+     */
     private boolean isGoingRotten = false;
+
+    /**
+     * Time to get good rotten.
+     */
     private int rottenTime = 5;
 
     public boolean isRotten() {
