48f67d40bd2a686b5c296385c22d27b5c9e97855
pavelsitnikov <pavelsitn@gmail.com>
2018-10-04 06:53:34 +0300

    добавлены животные

---
+++ src/ru/milandr/courses/farm/sitnikov/animals/Chicken.java
@@ -0,0 +1,17 @@
+package ru.milandr.courses.farm.sitnikov.animals;
+
+import ru.milandr.courses.farm.Animal;
+import ru.milandr.courses.farm.Good;
+import ru.milandr.courses.farm.sitnikov.goods.Egg;
+
+public class Chicken implements Animal {
+    @Override
+    public void produceSound() {
+        System.out.println("chick");
+    }
+
+    @Override
+    public Good produceGoods() {
+        return new Egg();
+    }
+}
---
+++ src/ru/milandr/courses/farm/sitnikov/animals/Cow.java
@@ -0,0 +1,17 @@
+package ru.milandr.courses.farm.sitnikov.animals;
+
+import ru.milandr.courses.farm.Animal;
+import ru.milandr.courses.farm.Good;
+import ru.milandr.courses.farm.sitnikov.goods.Milk;
+
+public class Cow implements Animal {
+    @Override
+    public void produceSound() {
+        System.out.println("mooo");
+    }
+
+    @Override
+    public Good produceGoods() {
+        return new Milk();
+    }
+}
