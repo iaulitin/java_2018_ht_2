d1cd4cac567551dce608ef6dca104584657ad6a8
IgorOzherelev <43535311+IgorOzherelev@users.noreply.github.com>
2018-10-06 23:03:52 +0300

    changed double to long

--- src/ru/milandr/courses/farm/ozherelev/Honey.java
+++ src/ru/milandr/courses/farm/ozherelev/Honey.java
@@ -3,7 +3,7 @@ package ru.milandr.courses.farm.ozherelev;
 public class Honey extends AnimalGoods {
     public Honey()
     {
-        this.setRottenTime(3.00);
+        this.setRottenTime(3000);
     }
 
     @Override
