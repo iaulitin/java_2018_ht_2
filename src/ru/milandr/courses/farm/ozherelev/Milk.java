b123853b9a759164b9606d3a035956655c0d2621
IgorOzherelev <43535311+IgorOzherelev@users.noreply.github.com>
2018-10-06 23:04:17 +0300

    changed double to long

--- src/ru/milandr/courses/farm/ozherelev/Milk.java
+++ src/ru/milandr/courses/farm/ozherelev/Milk.java
@@ -3,7 +3,7 @@ package ru.milandr.courses.farm.ozherelev;
 public class Milk extends AnimalGoods {
     public Milk()
     {
-        this.setRottenTime(2.00);
+        this.setRottenTime(2000);
     }
 
     @Override
